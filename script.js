8b1a1b8830e76db7e9aac4414423a0e96c119e8d
incolore9 <hansol.woff@gmail.com>
2024-10-18 23:29:08 +0900

    다크모드 실습, 조건문

---
+++ script.js
@@ -0,0 +1,13 @@
+const 다크모드버튼 = document.querySelector(".btn");
+let 클릭횟수 = 0;
+
+다크모드버튼.addEventListener("click", function () {
+  클릭횟수 = 클릭횟수 + 1;
+  if (클릭횟수 % 2 == 1) {
+    document.getElementsByClassName("btn")[0].innerHTML = "lightMode 😎";
+    document.getElementsByClassName("bg")[0].classList.add("blackBg");
+  } else {
+    document.getElementsByClassName("btn")[0].innerHTML = "darkMode 😴";
+    document.getElementsByClassName("bg")[0].classList.remove("blackBg");
+  }
+});
